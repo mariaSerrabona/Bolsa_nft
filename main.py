ab056c617a7c27011f97ce9bced1a39817b3931e
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-05-08 23:16:17 +0200

    intento fallido de anuncio

--- main.py
+++ main.py
@@ -75,12 +75,22 @@ class nft:
     name='1'
     precio=8402.38
     descripcion='nft bonito'
+    vendedor='userA'
 
-    def __init__(self,tipo_riesgo, name, precio, descripcion):
+    def __init__(self,tipo_riesgo, name, precio, descripcion, vendedor):
         self.tipo_riesgo=tipo_riesgo
         self.name=name
         self.precio=precio
         self.descripcion=descripcion
+        self.tipo_riesgo=vendedor
+
+    #se revuelve toda la información de un nft
+    def info_nft(self):
+
+        print('NFT con un riesgo: '+self.tipo_riesgo)
+        print('Nombre del NFT: '+self.name)
+        print('precio del NFT: '+self. precio)
+        print('Detalles del NFT: '+self.descripcion)
 
 # if __name__ == '__main__':
 
