a7ae6f74a7c260d051fea97d1983071a3d6f8d89
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-05-10 00:05:08 +0200

    probando hilos pero no funciona

--- código/nft.py
+++ código/nft.py
@@ -1,22 +1,50 @@
-class nft:
 
-    tipo_riesgo=['alto', 'bajo', 'medio']
-    name='1'
-    precio=8402.38
-    descripcion='nft bonito'
-    vendedor='userA'
+import threading
 
-    def __init__(self,tipo_riesgo, name, precio, descripcion, vendedor):
-        self.tipo_riesgo=tipo_riesgo
+#la base de los hilos es generarlos primero, y generar una función con cada una de las opciones que el hilo puede realizar.
+
+#en este caso, un NFT puede relizar la opción de ser rechazado, entonces se cierra el hilo y no se vuelve a mostrar
+                            #o bien puede ser aceptado entonces se tendrá que enviar un mensaje al vendedor notificando la venta
+
+
+
+class nft(threading.Thread):
+
+    semaforo=threading.Lock() #determina el bloqueo del hilo para que dos hilos no se puedan superponer
+    estado=[]   #se conoce el estado de cada hilo, si el nft se quiere comrpar o descartar
+
+    #constructor de la clase nft
+    def __init__(self, name, tipo_riesgo, precio, descripcion):
         self.name=name
+        self.tipo_riesgo=tipo_riesgo
         self.precio=precio
         self.descripcion=descripcion
-        self.tipo_riesgo=vendedor
 
-    #se revuelve toda la información de un nft
-    def info_nft(self):
+    def descartar(self): #función que se ejecutará cuando un nft sea descartado por el comprador
+        nft.estado[self.name] = 'siguiente'
+        nft.semaforo.release() #se deja el nft
 
-        print('NFT con un riesgo: '+self.tipo_riesgo)
-        print('Nombre del NFT: '+self.name)
-        print('precio del NFT: '+self. precio)
-        print('Detalles del NFT: '+self.descripcion)
+    def comprar(self):  #función que se ejecutará cuando un nft se quiera comprar
+        nft.estado[self.name] = 'comprar'
+        print('se ha comprado el nft: '+self.name)
+
+
+
+
+if __name__=="__main__":
+    N=input('Numero de anuncios que se quiere: ')
+    N=int(N)
+
+    name=input('nombre: ')
+    tipo_riesgo=input('tipo_riesgo: ')
+    precio=input('precio: ')
+    descripcion=input('descripcion: ')
+    lista=[]
+    for i in range(N):
+        lista.append(nft(name, tipo_riesgo, precio, descripcion)) #AGREGA UN FILOSOFO A LA LISTA
+
+    for f in lista:
+        f.start() #ES EQUIVALENTE A RUN()
+
+    for f in lista:
+        f.join() #BLOQUEA HASTA QUE TERMINA EL THREAD
